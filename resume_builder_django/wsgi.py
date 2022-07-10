015a29b028526d1ef779bceb2a955dc0c8a59f20
victorsmart <victchubxy@gmail.com>
2022-07-10 05:44:29 -0700

    fdgfhg

--- resume_builder_django/wsgi.py
+++ resume_builder_django/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for resume_builder project.
+WSGI config for resume_builder_django project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'resume_builder.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'resume_builder_django.settings')
 
 application = get_wsgi_application()
